029d9f30cd80fe1beda042936e4f8a665972ca1e
Eapu <elena.andresprado@gmail.com>
2017-10-24 18:20:50 +0100

    first commit

---
+++ orders/migrations/0002_auto_20171023_2316.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='order',
+            name='address',
+        ),
+        migrations.RemoveField(
+            model_name='order',
+            name='city',
+        ),
+        migrations.RemoveField(
+            model_name='order',
+            name='postal_code',
+        ),
+    ]
